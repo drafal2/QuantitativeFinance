be642f40fdc659387b5c97e2586266d506152c8c
drafal2 <damianrafal1999@gmail.com>
2026-01-01 22:45:11 +0100

    Implementing binomial tree european option pricing

--- functions.py
+++ functions.py
@@ -24,10 +24,41 @@ def historical_volatility(ts, if_returns=False, if_alternative_approach=False):
         avg = np.average(ts)
         vol = np.sqrt(1/(n-1) * np.sum((ts - avg) ** 2))
                 
-    return vol 
+    return vol
+
+def eq_option_pricing_binomial_tree(s0, K, T, r, sigma, N):
+    """Function utilized in binomial tree option pricing
+
+    Args:
+        s0 (float): current underlying price
+        K (float): strike price
+        T (float): maturity of option in years
+        r (float): risk-free rate
+        sigma (float): volatility of an underlying
+        N (int): number of time steps from 0 to T
+
+    Returns:
+        float: option price
+        np.ndarray: array with binomial tree of underlying prices
+    """
+    
+    dt = T / N
+    u = np.exp(sigma * np.sqrt(dt))
+    d = 1 / u
+    p = (np.exp(r * dt) - d) / (u - d)
+    discount = np.exp(-r * dt)
+
+    s_tree = s0 * (u ** np.arange(N, -1, -1)) * (d ** np.arange(0, N + 1, 1))
+    V = np.maximum(s_tree - K, 0)
+
+    for i in range(int(N) - 1, -1, -1):
+        V = discount * (p * V[:-1] + (1 - p) * V[1:])
+        
+    return V[0], s_tree
+
+def main():
+    return None
 
 
 if __name__ == '__main__':
-    x = np.array([15, 13, 17, 20, 27])
-    ret = historical_volatility(x, False, True)
-    print(ret)
+    main()
